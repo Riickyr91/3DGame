1718f18b210427f8b3502fc8f80de704de0fc623
Ricardo Manuel Ruiz Díaz <riicky_r91@hotmail.es>
2020-05-12 22:02:08 +0200

    C0005 - Neuronal Network

--- GeneticKartGame/Assets/CarController.cs
+++ GeneticKartGame/Assets/CarController.cs
@@ -2,56 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(NNet))]
 public class CarController : MonoBehaviour
 {
-    private Vector3 startPosition;      // Start position to reset the car
-    private Vector3 startRotation;      // Start rotation to reset the car
+    private Vector3 startPosition, startRotation;
+    private NNet network;
 
-    [Range(-1f, 1f)]
-    public float acceleration, turn;    //Acceleration and turning value
+    [Range(-1f,1f)]
+    public float a,t;
 
-    public float timeSinceStart = 0f;   // Check if the car has been idle for so much time and must be reset
+    public float timeSinceStart = 0f;
 
     [Header("Fitness")]
-    public float overallFitness;                // Fitness value for genetic algorithm
-    public float distanceMultiplier = 1.4f;     // Distance importance for fitness function
-    public float avgSpeedMultiplier = 0.2f;     // Speed importance for fitness function
-    public float sensorMultiplier = 0.1f;       // Sensor importance for fitness function (stay in the middle of the street)
+    public float overallFitness;
+    public float distanceMultipler = 1.4f;
+    public float avgSpeedMultiplier = 0.2f;
+    public float sensorMultiplier = 0.1f;
+
+    [Header("Network Options")]
+    public int LAYERS = 1;
+    public int NEURONS = 10;
 
     private Vector3 lastPosition;
     private float totalDistanceTravelled;
     private float avgSpeed;
 
-    private float aSensor, bSensor, cSensor;
+    private float aSensor,bSensor,cSensor;
 
-
-    public void Update()
-    {
-        getHumanInput();
-    }
-
-    private void getHumanInput()
-    {
-        float h = Input.GetAxis("Horizontal");
-        turn += 0.02f * h;
-
-        float v = Input.GetAxis("Vertical");
-        acceleration += 0.2f * v;
-
-        if (turn > 1) turn = 1;
-        if (turn < -1) turn = -1;
-        if (acceleration > 1) acceleration = 1;
-        if (acceleration < -1) acceleration = -1;
-    }
-
-    private void Awake()
-    {
+    private void Awake() {
         startPosition = transform.position;
         startRotation = transform.eulerAngles;
+        network = GetComponent<NNet>();
+
+        //TEST CODE
+        network.Initialise(LAYERS, NEURONS);
     }
 
-    public void Reset()
-    {
+    public void Reset() {
+
+        //TEST CODE
+        network.Initialise(LAYERS, NEURONS);
+
         timeSinceStart = 0f;
         totalDistanceTravelled = 0f;
         avgSpeed = 0f;
@@ -59,102 +50,89 @@ public class CarController : MonoBehaviour
         overallFitness = 0f;
         transform.position = startPosition;
         transform.eulerAngles = startRotation;
-        acceleration = 0;
-        turn = 0;
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
+    private void OnCollisionEnter (Collision collision) {
         Reset();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
+
         InputSensors();
         lastPosition = transform.position;
 
-        // Neural network code here
-        MoveCar(acceleration, turn);
+
+        (a, t) = network.RunNetwork(aSensor, bSensor, cSensor);
+
+
+        MoveCar(a,t);
 
         timeSinceStart += Time.deltaTime;
 
         CalculateFitness();
 
-        //a=0
-        //t=0
+        //a = 0;
+        //t = 0;
+
+
     }
 
 
-    private void CalculateFitness()
-    {
-        // Distance contribution
-        totalDistanceTravelled += Vector3.Distance(transform.position, lastPosition);
-        float distanceContribution = distanceMultiplier * totalDistanceTravelled;
+    private void CalculateFitness() {
 
-        // Speed contribution
-        avgSpeed = totalDistanceTravelled / timeSinceStart;
-        float speedContribution = avgSpeedMultiplier * avgSpeed;
+        totalDistanceTravelled += Vector3.Distance(transform.position,lastPosition);
+        avgSpeed = totalDistanceTravelled/timeSinceStart;
 
-        // Sensor contribution
-        float sensorContribution = sensorMultiplier * (aSensor + bSensor + cSensor) / 3;
+       overallFitness = (totalDistanceTravelled*distanceMultipler)+(avgSpeed*avgSpeedMultiplier)+(((aSensor+bSensor+cSensor)/3)*sensorMultiplier);
 
-        overallFitness = distanceContribution + speedContribution + sensorContribution;
-
-        if (timeSinceStart > 20 && overallFitness < 40)
-        {
+        if (timeSinceStart > 20 && overallFitness < 40) {
             Reset();
         }
 
-        if (overallFitness >= 1000)
-        {
-            // Saves the network to a JSON
+        if (overallFitness >= 1000) {
+            //Saves network to a JSON
             Reset();
         }
+
     }
 
+    private void InputSensors() {
 
-    private void InputSensors()
-    {
-        Vector3 a = (transform.forward + transform.right); // Right 45º angle sensor.
-        Vector3 b = transform.forward;                     // Forward sensor.
-        Vector3 c = (transform.forward - transform.right); // Left 45º angle sensor.
+        Vector3 a = (transform.forward+transform.right);
+        Vector3 b = (transform.forward);
+        Vector3 c = (transform.forward-transform.right);
 
-        Ray r = new Ray(transform.position, a);
+        Ray r = new Ray(transform.position,a);
         RaycastHit hit;
 
-        if(Physics.Raycast(r, out hit))
-        {
-            aSensor = hit.distance / 20;
-            //print("A sensor: " + aSensor);
+        if (Physics.Raycast(r, out hit)) {
+            aSensor = hit.distance/20;
+            Debug.DrawLine(r.origin, hit.point, Color.red);
         }
 
         r.direction = b;
-        if (Physics.Raycast(r, out hit))
-        {
-            bSensor = hit.distance / 20;
-            //print("B sensor: " + bSensor);
+
+        if (Physics.Raycast(r, out hit)) {
+            bSensor = hit.distance/20;
+            Debug.DrawLine(r.origin, hit.point, Color.red);
         }
 
         r.direction = c;
-        if (Physics.Raycast(r, out hit))
-        {
-            cSensor = hit.distance / 20;
-            //print("C sensor: " + cSensor);
+
+        if (Physics.Raycast(r, out hit)) {
+            cSensor = hit.distance/20;
+            Debug.DrawLine(r.origin, hit.point, Color.red);
         }
+
     }
 
-
-    // v = vertical movement  h = horizontal movement
     private Vector3 inp;
-    public void MoveCar(float v, float h)
-    {
-        inp = Vector3.Lerp(Vector3.zero, new Vector3(0, 0, v * 11.4f), 0.02f); // Make a linear Interpolation between two vector
-        inp = transform.TransformDirection(inp);    // Transform the inp vector in the local system of the car to ensure that
-                                                    // the car go forward.
+    public void MoveCar (float v, float h) {
+        inp = Vector3.Lerp(Vector3.zero,new Vector3(0,0,v*11.4f),0.02f);
+        inp = transform.TransformDirection(inp);
         transform.position += inp;
 
-        transform.eulerAngles += new Vector3(0, h*90*0.02f, 0); // h is the number of times we turn left or right
+        transform.eulerAngles += new Vector3(0, (h*90)*0.02f,0);
     }
-
 
 }
