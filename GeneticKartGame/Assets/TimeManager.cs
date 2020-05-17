b6e96895a6d02057d78093e3e1f5e21e1287cd18
Ricardo Manuel Ruiz Díaz <riicky_r91@hotmail.es>
2020-05-17 18:54:31 +0200

    C0029 - Slider IAtrainning

---
+++ GeneticKartGame/Assets/TimeManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour
+{
+    public Slider timeSlider;
+
+    public void timeUpdate()
+    {
+        Time.timeScale = timeSlider.value;
+    }
+}
