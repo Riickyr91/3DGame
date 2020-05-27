737ffc9b8c9f8a90e9d9c4d00687c8d3049322f9
Ricardo Manuel Ruiz Díaz <riicky_r91@hotmail.es>
2020-05-27 20:25:43 +0200

    C0038 - IASettings modified

--- GeneticKartGame/Assets/Scripts/Menus/IASettingsMenu.cs
+++ GeneticKartGame/Assets/Scripts/Menus/IASettingsMenu.cs
@@ -9,6 +9,10 @@ public class IASettingsMenu : MonoBehaviour
     public Slider maxGenerationsSlider;
     public Slider numEliteSlider;
     public Slider bestPercentegeSlider;
+    public Text textPopulation;
+    public Text textMaxGenerations;
+    public Text textNumElite;
+    public Text textBestPercentage;
 
     public void Start()
     {
@@ -26,24 +30,28 @@ public class IASettingsMenu : MonoBehaviour
     public void UpdatePopulation()
     {
         GameManager.iaSettings.populationSize = (int)populationSizeSlider.value;
+        textPopulation.text = "" + (int)populationSizeSlider.value;
         SaveSystem.Save(PathManager.iaSettingsPath, GameManager.iaSettings);
     }
 
     public void UpdateMaxGeneration()
     {
         GameManager.iaSettings.maxGenerations = (int)maxGenerationsSlider.value;
+        textMaxGenerations.text = "" + (int)maxGenerationsSlider.value;
         SaveSystem.Save(PathManager.iaSettingsPath, GameManager.iaSettings);
     }
 
     public void UpdateNumElite()
     {
         GameManager.iaSettings.numElite = (int)numEliteSlider.value;
+        textNumElite.text = "" + (int)numEliteSlider.value;
         SaveSystem.Save(PathManager.iaSettingsPath, GameManager.iaSettings);
     }
 
     public void UpdateBestPercentage()
     {
         GameManager.iaSettings.bestPercentage = bestPercentegeSlider.value;
+        textBestPercentage.text = "" + (int)(bestPercentegeSlider.value * 100);
         SaveSystem.Save(PathManager.iaSettingsPath, GameManager.iaSettings);
     }
 
